d9615c3bc50848e32b79d55ccb48754bbd8c557a
mateistefan13 <r.mateistefan@gmail.com>
2018-12-18 13:33:31 +0200

    Update Mare Decembrie (renuntare la obiectul c, dar inca isi trage datele din fisiere);

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 	Tool care compara daca o sala a pastrat in timp coordonatele locurilor, folosind timpul de referinta T0. Formatul folosit pentru identificarea locurilor este (zona, rand loc).
 
-	Tool-ul are la baza o aplicatie JS care importa fisiere (prin NodeJS) continand obiecte cu coordonatele locurilor.
-
-	Pentru a vedea in browser locurile care si-au schimbat coordonatele, am folosit compilatorul Browserify:
-		#acesta uneste datele de intrare (sala1_t0.js, sala1_t0.js) cu algoritmul de verificare (algoritm_verifiare.js) in bundle.js
-		#bundle.js este folosit de catre fisierul html pentru a afisa in consola locurile cu coordonate neechivalente
+	Tool-ul are la baza o aplicatie JS care importa fisiere continand obiecte cu coordonatele locurilor.
 
 
 Algoritmul de verificare:
@@ -12,21 +8,14 @@ Algoritmul de verificare:
 	-Avem obiectele: 
 		--"a" = obiectul ale carui proprietati au coordonatele locurilor initiale, coorodnatele de referinta (Pozitionare Initiala)
 		--"b" = obiectul ale carui proprietati au coordonatele locurilor noi, care trebuie confruntate (Pozitionare Noua)
-		--"c" = obiectul colector in care vor fi introduse atributele coordonatelor care nu sunt egale, inclusiv cheia lor (Identificatorul de Probleme)
-
-	- Folosim NodeJS pentru a importa datele de intrare. Fisierele importate contin obiectele, pe care le stocam aici in variabilele a si b.
 	
 	- IMPORTANT!!! atat datele din acest fisier, cat si obiectele din fisierele importate trebuie sa pastreze denumirea de "a" si "b" pentru ca tool-ul sa functioneze
-
-	- IMPORTANT!!! Daca nu se compara key-uri identice (ex, A_1_1 din sala_t0 cu A_1_1 din sala_t1), va da eroare!!! Trebuie comparate key-uri identice!!! 
-	
-	- REMINDER: Outputul (ex, A_5_5: [ 400, 400, 430, 430 ]) are structura A_n_n:[coordonata a, coordonata a, coordonata b, coordonata b] => Primele 2 sunt cele bune!!!
 
 Datele de intrare:
 	
 	- Sunt continute in fisierele sala1_t0.js si sala1_t0.js
 
-	- Pentru ca algoritmul sa functioneze, trebuie pastrate numele variabilelor si ale obiectelor (a, b, c)
+	- Pentru ca algoritmul sa functioneze, trebuie pastrate numele variabilelor si ale obiectelor (a, b)
 
 	- IMPORTANT!!! Fisierele cu aceste date de intrare trebuie sa contina obiecte cu date, nu datele simple!!!
     
@@ -34,3 +23,4 @@ Algoritm dinamic:
 
     - In momentul in care tool-ul recunoaste pozitionari diferite, le stocheaza in obiectul c si va procesa in mod dinamic cu ajutorul elementului Canvas cate un cerculet rosu pentru locul cu pozitia schimbata si unul verde pentru locul cu pozitia noua, indicand, de asemenea, cu o sageata cercurile pereche
     - Locurile care nu isi schimba pozitia sunt colorate cu negru
+	- Locurile nou aparute au culoarea verde 
--- algoritm_verificare.js
+++ algoritm_verificare.js
@@ -2,58 +2,35 @@
 /*	-Avem obiectele: 
 		--"a" = obiectul ale carui proprietati au coordonatele locurilor initiale, coorodnatele de referinta (Pozitionare Initiala)
 		--"b" = obiectul ale carui proprietati au coordonatele locurilor noi, care trebuie confruntate (Pozitionare Noua)
-		--"c" = obiectul colector in care vor fi introduse atributele coordonatelor care nu sunt egale, inclusiv cheia lor (Identificatorul de Probleme)
-
-	- Folosim NodeJS pentru a importa datele de intrare. Fisierele importate contin obiectele, pe care le stocam aici in variabilele a si b.
 	
 	- IMPORTANT!!! atat datele din acest fisier, cat si obiectele din fisierele importate trebuie sa pastreze denumirea de "a" si "b" pentru ca tool-ul sa functioneze
-
-	- IMPORTANT!!! Daca nu se compara key-uri identice (ex, A_1_1 din sala_t0 cu A_1_1 din sala_t1), va da eroare!!! Trebuie comparate key-uri identice!!! 
 	
-	- REMINDER: Outputul (ex, A_5_5: [ 400, 400, 430, 430 ]) are structura A_n_n:[coordonata a, coordonata a, coordonata b, coordonata b] => Primele 2 sunt cele bune!!!
 */
 
-var a = require('./sala1_t0'); 
-
-var b = require('./sala1_t1');
-	
-let c = {}
-Object.keys(a).forEach(function(key) {
-if (a[key][0] !== b[key][0] || a[key][1] !== b[key][1]) {
-		c[key] = a[key].concat(b[key]);
-	}
-})
-
-console.log(c);
+// pe variabilele a si b le pastrez daca vreau sa lucrez cu node 
+//var a = require('./sala1_t0'); 
+//var b = require('./sala1_t1');
 
 // Desenarea cercurilor pentru locurile initiale care nu se vor schimba, apar cu negru
 Object.keys(a).forEach(function(key) {
-		circle(a[key][0], a[key][1], "black");
+	circle(a[key][0], a[key][1], "black");
 })
 
 // Desenarea cercurilor pentru locurile noi care nu au corespondent in cele vechi
 Object.keys(b).forEach(function(key) {
-	if(key in b !== key in a) {
+	if (key in b !== key in a) {
 		circle(b[key][0], b[key][1], "green");
     }
 })
 
-
-// Iteratia care ajuta la dinamicizarea crearii de content, folosind datele introduse prin request
-// inainte foloseam for (var key in c) in loc de Object.keys(c)
-Object.keys(c).forEach(function(key) {
-	//dinamicizarea crearii cercului verde, locul cu pozitionare initiala
-	if (key in c === true) {
-		//vechile locuri sunt afisate cu rosu; fostul rosu era "rgba(255,0,0,0.5)"
-		circle(c[key][0], c[key][1], "red");
-	}
-	//dinamicizarea crearii cercului rosu, locul cu pozitionare noua
-    if (key in c === true) {
+Object.keys(a).forEach(function(key) {
+	if (a[key][0] !== b[key][0] || a[key][1] !== b[key][1]) {
+		//vechile locuri sunt afisate cu rosu;
+		circle(a[key][0], a[key][1], "red");
 		//noile locuri sunt afisate cu verde
-		circle(c[key][2], c[key][3], "green");
-    }
-	//dinamicizarea sagetii care pleaca de la locul gresit (cercul rosu) catre locul corespunzator (cercul verde)
-	if (key in c === true) {
-		line(c[key][2], c[key][3], c[key][0], c[key][1], "black");
+		circle(b[key][0], b[key][1], "green");
+		//dinamicizarea sagetii care pleaca de la locul vechi (cercul rosu) catre locul corespunzator (cercul verde)
+		line(b[key][0], b[key][1], a[key][0], a[key][1], "black");
 	}
 })
+
